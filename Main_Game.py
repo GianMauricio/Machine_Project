46e6be2509dab7b4082b51c13e39dfef78e16f86
GianMauricio <44298742+GianMauricio@users.noreply.github.com>
2018-11-14 21:04:49 +0800

    Added the Main game
    Base code will be used here for the actual game world

---
+++ Main_Game.py
@@ -0,0 +1,29 @@
+# This will serve as the main script, holding the over all structure of the game itself
+
+# Section 0 :: Import necessary modules
+import pygame as pyg
+import math
+import random
+
+# Section 1 :: Initialize literally everything
+pyg.init()
+pyg.display.init()
+width = 1280
+height = 720
+screen = pyg.display.set_mode((width, height))
+
+keys = [False, False, False, False]
+player_pos = [100, 100]
+
+running = True
+while running:
+    pyg.display.flip()
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            running = False
+# Section 2 :: Create the screen and the world in which the player will traverse
+
+# Section 3 :: Spawn essentials (Player, NPC's, etc.)
+# Section 4 :: Create game loop for beginning and ending
+# Section 5 :: Design control structure for player
+# Section 6 :: Design update structure for screen
