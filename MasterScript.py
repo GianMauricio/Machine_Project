6e440f1483afe3779514932ed3a04d1e918c89e2
GianMauricio <44298742+GianMauricio@users.noreply.github.com>
2018-11-26 15:39:37 +0800

    Updated The Master script
    This will be the new base for the game itself, currently waiting for Gabbi's VerF

--- MasterScript.py
+++ MasterScript.py
@@ -1,1 +1,20 @@
-# This script will handle all of the changes in the game state
+# This will serve as the main game loop for our multiplication operation problems
+# Much of this will be placeholder
+
+import pygame as pyg
+from random import randint as rin
+
+r2 = rin(10, 99)
+r1 = rin(10, 99)
+
+true_ans = r1 * r2
+
+print(r1, "*", r2, "=", true_ans)
+
+if true_ans >= 1000:
+    print("Now spawning 4 boxes")
+
+else:
+    print("Now spawning 3 boxes")
+
+"Begin the LOIF part of the script"
